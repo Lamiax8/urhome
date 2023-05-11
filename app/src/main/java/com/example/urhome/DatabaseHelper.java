2a80d3253083c0ecbb2c381f9ca1c11bb684b7fc
Lamiax8 <442200388@student.ksu.edu.sa>
2023-05-11 22:58:07 +0300

    lamiasEdit3

--- app/src/main/java/com/example/urhome/DatabaseHelper.java
+++ app/src/main/java/com/example/urhome/DatabaseHelper.java
@@ -6,14 +6,36 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHelper  extends SQLiteOpenHelper{
     public static final String databaseName = "SignLog.db";
+
+
+
+
+    public static final String APART_TABLE = "Apart_Table";
+    public static final String COLUMN_APART_NAME = "APART_NAME";
+    public static final String COLUMN_APART_PRICE = "APART_PRICE";
+    public static final String COLUMN_APART_LOCATION = "APART_LOCATION";
+
+    public static final String COLUMN_APART_ROOMS = "APART_ROOMS";
+    public static final String COLUMN_ID = "ID";
+
+
+
+
+
     public DatabaseHelper(@Nullable Context context) {
         super(context, "SignLog.db", null, 1);
     }
     @Override
     public void onCreate(SQLiteDatabase MyDatabase) {
         MyDatabase.execSQL("create Table users(email TEXT primary key, password TEXT)");
+        String createTableStatement = "Create TABLE " + APART_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_APART_NAME + " TEXT, " + COLUMN_APART_PRICE + " INT, " +COLUMN_APART_LOCATION + " TEXT, " + COLUMN_APART_ROOMS + " INT)" ;
+        MyDatabase.execSQL(createTableStatement);
     }
     @Override
     public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
@@ -51,6 +73,64 @@ public class DatabaseHelper  extends SQLiteOpenHelper{
     }
 
 
+
+
+    public boolean addOne(apart apartMod) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_APART_NAME, apartMod.getName());
+        cv.put(COLUMN_APART_PRICE, apartMod.getPrice());
+        cv.put(COLUMN_APART_LOCATION, apartMod.getLocation());
+        cv.put(COLUMN_APART_ROOMS, apartMod.getRooms());
+
+        long insert = db.insert(APART_TABLE, null, cv);
+        if (insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean DeleteOne(apart apartMod) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "Delete From " + APART_TABLE + " WHERE " + COLUMN_ID + " = " + apartMod.getId();
+        Cursor cursor = db.rawQuery(queryString, null);
+        if (cursor.moveToFirst()) {
+            return true;
+        } else {
+            // nothing happens. no one is added.
+            return false;
+        }
+        //close
+    }
+
+    public List<apart> getEveryone() {
+        List<apart> returnList = new ArrayList<>();
+        // get data from database
+        String queryString = "Select * from " + APART_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+        if (cursor.moveToFirst()) {
+            // loop through cursor results
+            do {
+                int AID = cursor.getInt(0);
+                String AName = cursor.getString(1);
+                int APrice = cursor.getInt(2);
+                String ALocation = cursor.getString(3);
+                int ARooms = cursor.getInt(4);
+
+                apart newApart = new apart(AID, AName, APrice ,ALocation,ARooms);
+                returnList.add(newApart);
+            } while (cursor.moveToNext());
+        } else {
+            // nothing happens. no one is added.
+        }
+        //close
+        cursor.close();
+        db.close();
+        return returnList;
+    }
 
 
 
